63fcd6224fd28dc705ed320e90b0356476137c95
ruboon-dej <ruboon_d@yahoo.com>
2021-01-28 09:43:27 +0700

    fixed bug

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -3,13 +3,13 @@ import math
 def calculate(fnum, process, snum):
     if process == "+":
         result = fnum + snum
-    elif process = "/":
+    elif process == "/":
         result = fnum / snum
-    elif process = "*":
+    elif process == "*":
       result = fnum * snum
-    elif process = "**"
+    elif process == "**"
         result = fnum * snum
-    elif process = "root":
+    elif process == "root":
         result = math.sqrt(fnum)
     else:
         result = "An Error has occured please try again."
