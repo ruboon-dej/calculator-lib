455f92657c538628b4ca9bbd88b870cbbeb9a91b
ruboon-dej <ruboon_d@yahoo.com>
2021-01-28 09:45:15 +0700

    try

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="calculator",
     packages=find_packages(include=["calculator"]),
-    version="0.1.0",
+    version="0.1.2",
     author="Ruboon Dej-Udom",
     license="MIT",
     test_suite="tests",
