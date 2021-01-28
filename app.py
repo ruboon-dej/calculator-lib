b6b33e985423d09f54400173d45e1b52eaa5bd0c
ruboon-dej <ruboon_d@yahoo.com>
2021-01-28 09:16:50 +0700

    added calculate function and gitignore

---
+++ app.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate(fnum, process, snum):
+    if process == "+":
+        result = fnum + snum
+    elif process = "/":
+        result = fnum / snum
+    elif process = "*":
+      result = fnum * snum
+    elif process = "**"
+        result = fnum * snum
+    elif process = "root":
+        result = math.sqrt(fnum)
+    else:
+        result = "An Error has occured please try again."
+    return result
+
